d34800377256a0cd93525d4b34464ab9198fd243
DarcyWJ <64890014+DarcyWJ@users.noreply.github.com>
2022-03-31 10:25:52 +1030

    Reverse Update

--- Reverse.cpp
+++ Reverse.cpp
@@ -9,15 +9,10 @@ int Reverse::reverseDigit(int value){
     if (value > 0)
     {
         intm = value % 10;
-        std::cout << "Intm Value: " << intm;
-        std::cout << "\n";
         rValue = (rValue * 10) + intm;
-        std::cout << "rValue: " << rValue;
-        std::cout << "\n";
         reverseDigit(value/10);
     }
     else{
-        std::cout << "Invalid Digit!\n";
         return value;
     }
     
@@ -27,7 +22,6 @@ std::string Reverse::reverseString(std::string letters){
     int l = letters.size();
     if (l == 0)
     {
-        std::cout << "Invalid string!\n";
         return letters;
     }
     else if (l > 0)
