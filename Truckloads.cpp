b8bfae10fdf5e36d9fa2bddca0fa2c7403d8bd21
DarcyWJ <64890014+DarcyWJ@users.noreply.github.com>
2022-03-31 15:31:51 +1030

    Revision

--- Truckloads.cpp
+++ Truckloads.cpp
@@ -6,6 +6,12 @@ Truckloads::Truckloads()
 }
 int Truckloads::numTrucks(int numCrates, int loadSize){
     
+    if (numCrates < 0 || loadSize < 0)
+    {
+        return trucks;
+    }
+    
+   
     if (numCrates <= loadSize)
     {
         trucks++;
