2c540a3a9ad179c2ce37a5f85feeefc0cb9bf3f3
DarcyWJ <64890014+DarcyWJ@users.noreply.github.com>
2022-04-02 10:54:35 +1030

    Efficient

--- EfficientTruckloads.cpp
+++ EfficientTruckloads.cpp
@@ -1,9 +1,46 @@
 #include "EfficientTruckloads.h"
+#include <vector>
+using namespace std;
 
-EfficientTruckloads::EfficientTruckloads(/* args */)
+vector<int> lookupTable;
+ 
+
+Truckloads::Truckloads()
 {
+    trucks = 0;
+}
+int Truckloads::numTrucks(int numCrates, int loadSize){
+    
+    if (numCrates <= 0 || loadSize <= 0)
+    {
+        return trucks;
+    }
+    if (numCrates <= loadSize)
+    {
+        trucks++;
+        //lookupTable[numCrates] = trucks;
+    }
+    else if (numCrates > loadSize)
+    {
+        //if (lookupTable.at(n) //not error/empty)
+       // {
+        //    next = lookupTable.at(n);
+        //    nextNext = lookupTable.at
+       // }
+        else if (numCrates%2 == 0) //i.e. even division with no remainder
+        {
+            numTrucks(numCrates/2, loadSize);
+            numTrucks(numCrates/2, loadSize);
+        }
+        else
+        {
+            numTrucks(numCrates/2, loadSize);
+            numTrucks((numCrates+1)/2, loadSize);
+        }
+    }
+    return trucks;
 }
 
-EfficientTruckloads::~EfficientTruckloads()
+Truckloads::~Truckloads()
 {
 }
