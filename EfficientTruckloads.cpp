dffb1e4f876012b20f2fa240a0825bb768d51d79
DarcyWJ <64890014+DarcyWJ@users.noreply.github.com>
2022-04-03 22:43:10 +0930

    Lookuptable update

--- EfficientTruckloads.cpp
+++ EfficientTruckloads.cpp
@@ -1,42 +1,52 @@
 #include "EfficientTruckloads.h"
 #include <vector>
+#include <iostream>
 using namespace std;
 
-vector<int> lookupTable;
- 
 
 EfficientTruckloads::EfficientTruckloads()
 {
-
+    trucks = 0;
 }
 int EfficientTruckloads::numTrucks(int numCrates, int loadSize){
     
+    if (lookupTable.size() < numCrates)
+    {
+        lookupTable.resize(numCrates+1);
+    }
+
     if (numCrates <= 0 || loadSize <= 0)
     {
-        return;
+        return lookupTable.at(numCrates);
     }
-    if (numCrates <= loadSize)
+    
+    if (lookupTable.at(numCrates) != 0)
     {
-        lookupTable[numCrates] = 1;
+        return lookupTable.at(numCrates);
     }
-    else if (numCrates > loadSize)
+   else if (lookupTable.at(numCrates) == 0)
     {
-        if (numCrates%2 == 0) //i.e. even division with no remainder
+        if (numCrates <= loadSize)
         {
-            lookupTable[numCrates] = lookupTable[numCrates/2]+lookupTable[numCrates/2];
-            numTrucks(numCrates/2, loadSize);
-            numTrucks(numCrates/2, loadSize);
+            return 1;
         }
-        else
+        else if (numCrates > loadSize)
         {
-            lookupTable[numCrates] = lookupTable[numCrates/2]+lookupTable[(numCrates+1)/2];
-            numTrucks(numCrates/2, loadSize);
-            numTrucks((numCrates+1)/2, loadSize);
+            if (numCrates%2 == 0) //i.e. even division with no remainder
+            {
+                lookupTable.at(numCrates)= numTrucks(numCrates/2, loadSize)+ numTrucks(numCrates/2, loadSize);
+            }
+            else
+            {
+                lookupTable.at(numCrates) = numTrucks(numCrates/2, loadSize)+ numTrucks((numCrates+1)/2, loadSize);
+            }
         }
-    }
-    return lookupTable[numCrates];
+
+}
+    return lookupTable.at(numCrates);
 }
 
 EfficientTruckloads::~EfficientTruckloads()
 {
+
 }
--- EfficientTruckloads.h
+++ EfficientTruckloads.h
@@ -1,8 +1,15 @@
+#ifndef EFFICIENTTRUCKLOADS_H
+#define EFFICIENTTRUCKLOADS_H
+
+#include <vector>
 class EfficientTruckloads
 {
 private:
+    int trucks;
+    std::vector<int> lookupTable;
 public:
     EfficientTruckloads();
     int numTrucks(int numCrates, int loadSize);
     ~EfficientTruckloads();
 };
+#endif
